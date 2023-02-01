2ecd15bcb3b4043d1bb8350c035fe319a63b158b
parth141093 <parthpatel1410@gmail.com>
2023-02-01 10:54:01 +0200

    excercise 1 complete

--- layout/index.php
+++ layout/index.php
@@ -11,5 +11,8 @@
     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo cumque minus similique id ad voluptatibus eligendi,
     excepturi assumenda vitae labore, dolores totam, deserunt iusto. Aspernatur odit at vero voluptas optio?
 </p>
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt aut tempore nihil natus non itaque error quo quod dicta nemo quisquam iste, soluta, ea veritatis officiis repellat ipsam deleniti veniam?
+</p>
 
 <? include "footer.php" ?>
